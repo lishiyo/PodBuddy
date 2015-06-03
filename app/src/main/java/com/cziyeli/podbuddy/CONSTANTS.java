d45168210f0f0d67913b5a742b9fade97f7c3482
Connie <c.ziye.li@gmail.com>
2015-06-02 22:20:07 -0400

    sets up three fragments for one activity

---
+++ app/src/main/java/com/cziyeli/podbuddy/CONSTANTS.java
@@ -0,0 +1,9 @@
+package com.cziyeli.podbuddy;
+
+/**
+ * Created by connieli on 6/2/15.
+ */
+public class CONSTANTS {
+    public static final String DEBUG_TAG = "myapp";
+    public static final int FRAGS_COUNT = 3;
+}
---
+++ app/src/main/java/com/cziyeli/podbuddy/fragments/PlaceholderFragment.java
@@ -0,0 +1,7 @@
+package com.cziyeli.podbuddy.fragments;
+
+/**
+ * Created by connieli on 6/2/15.
+ */
+public class PlaceholderFragment {
+}
